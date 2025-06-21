1f1ced126e49851225bae8679d7ca641ce87ba93
Treyser044 <treyser044@gmail.com>
2025-06-21 09:59:42 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,42 @@
+/*console.log('Gay');
+
+
+var student1 = 'GUY'
+let student2 = 'GEY'
+const student3 = 'GIY'
+
+/*console.log(student1);
+console.log(student2);
+console.log(student3);*/
+
+/*alert('ya ne gay 🧄')
+confirm('OSHIBAYESHSYA YA NE GAY 🤧')
+
+let name = prompt('YA NE GEEEEEY SKASHI SVOYO IMYA YA NAYDU TEBYA')
+let like = prompt('like')
+let dontlike = prompt('dontlike')
+ 
+
+console.log(name);
+console.log(like);
+console.log(dontlike);
+
+alert('Ozini lox deb oylaydigan isqirt ' + name + '\n unga yaraydigan odam' + like + 'Unga  yaramaydigan odam ' + dontlike )*/
+
+
+let name = prompt('ismizni kiriting')
+let Surname = prompt('familiyangizni kiriting')
+let age = prompt('yoshingizni kiriting')
+let like = prompt('yoqtirgan faningizni kiriting')
+let place = prompt('yashaydigan joyingizni kiriting')
+alert('malumotlaringizni kiritkaningiz uchun raxmat')
+
+console.log('Ism: ' + name);
+console.log('Familiya: ' + Surname);
+console.log('Yosh: ' + age);
+console.log('Yoqtirgan fan: ' + like);
+console.log('Yashaydigan manzil: ' + place);
+
+
+
+
